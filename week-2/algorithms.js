361d349020f37eb3891d53fec177a1aaaed1fc8d
kailin yang <kailinnnnn0408@gmail.com>
2023-09-12 16:13:06 +0800

    add advanced & change variable

--- week-2/algorithms.js
+++ week-2/algorithms.js
@@ -16,7 +16,7 @@ console.log(max([5, 2, 7, 1, 6])); // expected output: 7
 //
 //Assignment 2: Function and Object
 //Complete the function below to calculate the result of the passing object.
-function calculate(args) {
+function calculate1(args) {
   if (args.op === "+") {
     return args.n1 + args.n2;
   } else if (args.op === "-") {
@@ -25,9 +25,9 @@ function calculate(args) {
     return "Not supported";
   }
 }
-console.log(calculate({ n1: 2, n2: 3, op: "+" })); // expected output: 5
-console.log(calculate({ n1: 5, n2: 2, op: "-" })); // expected output: 3
-console.log(calculate({ n1: 1, n2: 6, op: "x" })); // expected output: 'Not supported'
+console.log(calculate1({ n1: 2, n2: 3, op: "+" })); // expected output: 5
+console.log(calculate1({ n1: 5, n2: 2, op: "-" })); // expected output: 3
+console.log(calculate1({ n1: 1, n2: 6, op: "x" })); // expected output: 'Not supported'
 //
 //
 //
@@ -36,14 +36,14 @@ console.log(calculate({ n1: 1, n2: 6, op: "x" })); // expected output: 'Not supp
 //Assignment 3: Function, Array, and Object
 //Complete the function below to calculate the total price of all products after applying a
 //discount.
-function calculate(data) {
+function calculate2(data) {
   let total = 0;
   for (let i = 0; i < data.products.length; i++) {
     total += data.products[i].price;
   }
   return total - total * data.discount;
 }
-const discountedPrice = calculate({
+const discountedPrice = calculate2({
   discount: 0.1,
   products: [
     {
@@ -61,3 +61,22 @@ const discountedPrice = calculate({
   ],
 });
 console.log(discountedPrice); // show the total price of all products after applying a discount
+//
+//
+//
+//
+//
+/*Assignment 5: Algorithm (Advanced Optional)
+Given an array of integers, return indices of the two numbers such that they add up to a
+specific target. You may assume that each input would have exactly one solution, and you
+may not use the same element twice.*/
+function twoSum(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    let diff = target - nums[i];
+    if (nums.indexOf(diff) !== -1 && nums.indexOf(diff) !== i) {
+      return [i, nums.indexOf(diff)];
+    }
+  }
+}
+
+console.log(twoSum([2, 7, 11, 15], 9));
--- week-2/app.js
+++ week-2/app.js
@@ -1,26 +1,26 @@
+//Click to Change Text.
 const title = document.querySelector(".section-top .title");
 const button = document.querySelector(".menu-btn");
 
-//Click to Change Text.
 title.addEventListener("click", () => {
   title.innerText = "Have a Good Time!";
 });
 
 //Click to Show/Close Menu
-let asideMenu = document.createElement("div");
+const asideMenu = document.createElement("div");
 asideMenu.classList.add("asideMenu");
 
-let ul = document.createElement("ul");
+const ul = document.createElement("ul");
 ul.classList.add("asideMenu_ul");
 asideMenu.appendChild(ul);
 
-let icon = document.createElement("i");
+const icon = document.createElement("i");
 icon.classList.add("fa-solid", "fa-xmark");
 asideMenu.appendChild(icon);
 
-let liNumber = 4;
+const liNumber = 4;
 for (let i = 0; i < liNumber; i++) {
-  let li = document.createElement("li");
+  const li = document.createElement("li");
   li.classList.add("asideMenu_ul_li", `asideMenu_ul_li-${liNumber}`);
   li.innerText = "Item " + (i + 1);
   ul.appendChild(li);
@@ -35,14 +35,14 @@ icon.addEventListener("click", () => {
 });
 
 //Click to Show More Content Boxes.
-let section = document.querySelector(".section-bottom");
-let content = document.createElement("div");
+const section = document.querySelector(".section-bottom");
+const content = document.createElement("div");
 content.classList.add("content");
-let belowDivNumber = 4;
+const belowDivNumber = 4;
 for (let i = 5; i < 5 + belowDivNumber; i++) {
-  let contentBox = document.createElement("div");
+  const contentBox = document.createElement("div");
   contentBox.classList.add(`content__box-${i}`, "hidden");
-  let boxContent = document.createElement("span");
+  const boxContent = document.createElement("span");
   boxContent.classList.add(`box-${i}__content`);
   boxContent.innerText = `Content Box ${i}`;
   contentBox.appendChild(boxContent);
@@ -50,9 +50,9 @@ for (let i = 5; i < 5 + belowDivNumber; i++) {
 }
 section.appendChild(content);
 
-let bottomBtn = document.querySelector(".section-bottom button");
+const bottomBtn = document.querySelector(".section-bottom button");
 bottomBtn.addEventListener("click", () => {
-  let hiddenDiv = document.querySelectorAll(".hidden");
+  const hiddenDiv = document.querySelectorAll(".hidden");
   hiddenDiv.forEach((div) => {
     div.classList.remove("hidden");
   });
