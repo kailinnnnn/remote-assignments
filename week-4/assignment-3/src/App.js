362fea058f5f14c282e45daef7b8ebf7e3599b92
kailin yang <kailinnnnn0408@gmail.com>
2023-09-25 16:47:22 +0800

    delete route

--- week-4/assignment-3/src/App.js
+++ week-4/assignment-3/src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
 import Homepage from "./pages/homepage";
-import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-      </Routes>
+      <Homepage />
     </div>
   );
 }
